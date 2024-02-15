56ef0e7ae92d3f135f496bd564053f467cd5d64a
MisterDefender <nikhilmukati123@gmail.com>
2024-02-15 13:40:01 +0530

    dev: ownable -> ownable2Step

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.20;
 
 // import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract Airdrop is Ownable {
+contract Airdrop is Ownable2Step {
     using SafeERC20 for IERC20;
 
     IERC20 public airdropToken;
